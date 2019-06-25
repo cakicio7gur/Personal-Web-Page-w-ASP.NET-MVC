0fdaa30f5856fb9ce9a198b067e6aa435b427b7d
Özgür <39501502+cakicio7gur@users.noreply.github.com>
2019-06-25 23:49:05 +0300

    Kullanıcı kayıt ve giriş sayfaları oluşturuldu.

---
+++ PersonalWebSite/PersonalWebSite/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWebSite.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
