6dece23d324511dc8a393fd325cc2000b07773bf
Özgür <39501502+cakicio7gur@users.noreply.github.com>
2019-07-10 17:33:41 +0300

    Blog sayfası için sayfalama işlemi yapıldı.

--- PersonalWebSite/PersonalWebSite/Controllers/BlogController.cs
+++ PersonalWebSite/PersonalWebSite/Controllers/BlogController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PersonalWebSite.Models;
-
+using PagedList;
+using PagedList.Mvc;
 namespace PersonalWebSite.Controllers
 {
     public class BlogController : Controller
     {
         Models.PersonalWebPageDBEntities db = new Models.PersonalWebPageDBEntities();
+        PersonalWebPageDBEntities Veri = new PersonalWebPageDBEntities();
 
-        public ActionResult Index()
+        public ActionResult Index(int? SayfaNo)
         {
-            var model = db.Makale.ToList();
-            return View(model);
+            int _sayfaNo = SayfaNo ?? 1;
+            var MakaleListesi = Veri.Makale.OrderByDescending(m =>m.makaleID).ToPagedList<Makale>(_sayfaNo, 3);
+            return View(MakaleListesi);
         }
         public ActionResult GetMakaleById(int id)
         {
@@ -27,6 +30,21 @@ namespace PersonalWebSite.Controllers
         {
             var blog = db.Makale.Where(x => x.kategoriID == kategoriID).ToList();
             return View("Index", blog);
+        }
+        public ActionResult GetMakaleByTarget(string target = null)
+        {
+            List<Models.Makale> Makale = new List<Models.Makale>();
+            foreach (var makale in db.Makale.ToList())
+            {
+                foreach (var icerik in makale.MakaleDetay.icerik.ToList())
+                {
+                    if (icerik.ToString().Contains(target)==true)
+                    {
+                        Makale.Add(makale);
+                    }
+                }
+            }
+            return RedirectToAction("Index", Makale);
         }
 
     }
